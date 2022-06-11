78d78682e06b985a9ed127e543a04e5080ee5aa6
tabeaki <tabeitakaaki@gmail.com>
2022-06-12 00:32:37 +0900

    title update

--- pages/components/Seo.tsx
+++ pages/components/Seo.tsx
@@ -19,7 +19,7 @@ const Seo: VFC<MetaData> = ({
   pageImgWidth,
   pageImgHeight
 }) => {
-  const defaultTitle = 'Astar Sign Witch'
+  const defaultTitle = 'Witch Verse Labs-Opti'
   const defaultDescription = 'Zodiac Signs x Witches x Echi-Echi = Strongest The project is aimed at a crypto-native IP. It is planned to be a manga and a game in the future. Please buy your constellation witch or your guess. We also plan to airdrop NFT in other chains if you are holding.'
 
   const title = defaultTitle
