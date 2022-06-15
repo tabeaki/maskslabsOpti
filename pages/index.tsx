b81863ee4d67a8c5a935e9dbb678ed7c7c305c86
tabeaki <tabeitakaaki@gmail.com>
2022-06-15 23:27:26 +0900

    update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ const Home: NextPage = () => {
     <div className="flex flex-wrap buttom justify-center bg-black bg-center bg-cover">
       <div className="m-16 px-2 py-20 lg:px-20 lg:py-4 border-double border-8 rounded-md bg-black text-center bg-center bg-contain bg-no-repeat">
           <h3 className="text-xs lg:text-4xl text-white font-semibold ">Optimism Mask NFT</h3>
-          <h1 className="text-sm lg:text-2xl pt-1 text-white font-semibold ">FREE MINT</h1>
+          <h1 className="text-sm lg:text-2xl pt-1 text-white font-semibold ">FREE MINT : Up to 5 in the wallet</h1>
           <h1 className="text-base lg:text-5xl pt-1 pb-2 text-white font-semibold "> {mintNum} / 6000</h1>        
           { paused && <h3 className="text-lg lg:text-3xl pt-1 text-white font-semibold ">Wait until the sale</h3>}
           { (mintNum <6000) && <button id="mintButton" className="px-4 py-2 my-1 sm:text-lg lg:text-2xl text-white font-semibold rounded bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900" onClick={MetaMuskConnect}>FREE MINT</button>}
